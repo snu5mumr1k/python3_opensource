51ca6feb23651145ebce110d792b3c94f1254f01
Andrey Feofilaktov <andarpolis@gmail.com>
2019-03-13 20:43:51 +0300

    app with one quit button in the center

--- scripts/quit_only.py
+++ scripts/quit_only.py
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from tkinter import Tk
+from argparse import ArgumentParser
+from tkinter import Tk, Button, CENTER
+import sys
+
+
+def run(args):
+    root = Tk()
+    root.attributes("-fullscreen", True)
+
+    button = Button(text="Quit", command=lambda: sys.exit(0))
+    button.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    root.mainloop()
 
 
 if __name__ == "__main__":
-    root = Tk()
-    root.mainloop()
+    parser = ArgumentParser()
+    args = parser.parse_args()
+
+    run(args)
